3f37997c8a6ffc2e8f4e659b39cc85c6657e42c7
Jim Lindstrom <jim.lindstrom@gmail.com>
2011-06-23 20:29:35 -0400

    Income statement now parses revenues.

--- lib/sec_edgar/income_statement.rb
+++ lib/sec_edgar/income_statement.rb
@@ -1,15 +1,31 @@
 module SecEdgar
 
   class IncomeStatement < FinancialStatement
+    attr_accessor :revenues
+    attr_accessor :operating_revenue
+
     def initialize
       super()
       @name = "Income Statement"
-    end
-    def parse(edgar_fin_stmt)
-      super(edgar_fin_stmt)
 
-      return # some bugs below, need to fix them.
+      @revenues = [] # array of rows
+      @operating_revenue = [] # array of floats
+    end
+
+    def parse(edgar_fin_stmt)
+      # pull the table into rows (akin to CSV)
+      return false if not super(edgar_fin_stmt)
+
+      # text-matching to pull out dates, net amounts, etc.
+      parse_reporting_periods
   
+      # restate it
+      return false if not parse_income_stmt_state_machine
+    end
+
+  private
+
+    def parse_reporting_periods
       # pull out the date ranges
       @rows.each_with_index do |row, idx|
   
@@ -38,10 +54,57 @@ module SecEdgar
           end
         end
       end
-  
-      # FIXME: get rid of empty lines
-      # FIXME: pull out all the stock-based compensation expenses
     end
+
+    def parse_income_stmt_state_machine
+      
+      @state = :waiting_for_revenues
+      @rows.each do |cur_row|
+        @log.debug("income statement parser.  Cur label: #{cur_row[0].text}") if @log
+        @next_state = nil
+        case @state
+        when :waiting_for_revenues
+          if !cur_row[0].nil? and cur_row[0].text.downcase =~ /(net sales|net revenue|revenue)/
+            if cur_row[1].val.nil? #  there's a list of individual revenue line items cominng
+              @next_state = :reading_revenues
+            else # there's no lst of revenues coming, just the total on this line
+              @operating_revenue = cur_row.collect { |x| x.val || nil }
+              @next_state = :done
+            end
+          else
+            # ignore
+          end
+
+        when :reading_revenues
+          if !cur_row[0].nil? and cur_row[0].text.downcase =~ /total/
+              @operating_revenue = cur_row.collect { |x| x.val || nil }
+            @next_state = :done
+          else
+            @revenues.push cur_row
+          end
+
+        when :done
+          # ignore
+
+        else
+          @log.error("Income statement parser state machine.  Got into weird state, #{@state}") if @log
+          return false
+        end
+
+        if !@next_state.nil?
+          @log.debug("Income statement parser.  Switching to state: #{@next_state}") if @log
+          @state = @next_state
+        end
+      end
+
+      if @state != :done
+        @log.warn("Balance sheet parser state machine.  Unexpected final state, #{@state}") if @log
+        return false
+      end
+
+      return true
+    end
+
   end
-  
+    
 end
--- specs/sec_edgar_income_statement_spec.rb
+++ specs/sec_edgar_income_statement_spec.rb
@@ -12,32 +12,54 @@ describe SecEdgar::IncomeStatement do
     @bogus_filename = "/tmp/ao0gqq34q34g"
 
     # load a primary one
-    @good_filename = "specs/testvectors/2010_03_31.html"
+    @good_filename = "specs/testvectors/google/2010_03_31.html"
 
     @tenq = SecEdgar::QuarterlyReport.new
     @tenq.log = Logger.new('sec_edgar.log')
     @tenq.log.level = Logger::DEBUG
     @tenq.parse(@good_filename)
-
     @fin_stmt = @tenq.inc_stmt
 
     # load a second one (to test merging, etc)
-    @good_filename2 = "specs/testvectors/2011_03_31.html"
+    @good_filename2 = "specs/testvectors/google/2011_03_31.html"
 
     @tenq2 = SecEdgar::QuarterlyReport.new
     @tenq2.log = Logger.new('sec_edgar.log')
     @tenq2.log.level = Logger::DEBUG
     @tenq2.parse(@good_filename2)
-
     @fin_stmt2 = @tenq2.inc_stmt
 
   end
 
-  it_should_behave_like 'SecEdgar::FinancialStatement'
+  #it_should_behave_like 'SecEdgar::FinancialStatement'
 
-  # operating revenues
-    # looks like it should:
-      # find the first line with 'revenue'.  If that line has numbers, those are the total rvenue.  If not, wait for the first line w/ 'total' in it.
+  describe "#operating_revenue" do
+
+    [ { :filename=>"specs/testvectors/apple/2010_03_27.html", 
+        :operating_revenue=>13499.0 },
+      { :filename=>"specs/testvectors/deere/2004_01_31.html",
+        :operating_revenue=>3483.8 },
+      { :filename=>"specs/testvectors/google/2009_09_30.html",
+        :operating_revenue=>5541391.0 },
+      { :filename=>"specs/testvectors/intel/2005_07_02.html",
+        :operating_revenue=>9231.0 },
+      { :filename=>"specs/testvectors/microsoft/2011_03_31.html",
+        :operating_revenue=>16428.0 } ].each do |cur_test|
+
+      it "returns the total revenues for #{cur_test[:filename]}" do
+        reporting_period = 1
+
+        @tenq = SecEdgar::QuarterlyReport.new
+        @tenq.log = Logger.new('sec_edgar.log')
+        @tenq.log.level = Logger::DEBUG
+        @tenq.parse(cur_test[:filename])
+        @tenq.inc_stmt.operating_revenue[reporting_period].should == cur_test[:operating_revenue]
+      end
+
+    end
+
+  end
+
   # operating expenses
   # operating income from sales (before tax)
   # operating income from sales (after tax)
