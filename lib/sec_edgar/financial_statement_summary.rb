739cd1a81fe704af2b6c7fc4bec872b9a9f3268a
Jim Lindstrom <jim.lindstrom@gmail.com>
2011-06-26 10:48:25 -0400

    Can merge FSS's now. =)

--- lib/sec_edgar/financial_statement_summary.rb
+++ lib/sec_edgar/financial_statement_summary.rb
@@ -3,12 +3,14 @@ module SecEdgar
   class FinancialStatementSummary
     attr_accessor :report_dates
 
+    # FIXME: all of this should be in a hash so that it can be more easily iterated over
+
     # balance sheet
     attr_accessor :oa, :ol, :noa
     attr_accessor :fa, :fl, :nfa
     attr_accessor :cse
 
-    # Balance Sheet Analysis
+    # Balance Sheet Analysis (FIXME: this should be calc'ed here, so that it can be merged)
     attr_accessor :composition_ratio
     attr_accessor :noa_growth
     attr_accessor :cse_growth
@@ -21,14 +23,14 @@ module SecEdgar
     attr_accessor :financing_income
     attr_accessor :net_income
 
-    # Income Statement Margins
+    # Income Statement Margins (FIXME: this should be calc'ed here, so that it can be merged)
     attr_accessor :gm
     attr_accessor :sales_pm
     attr_accessor :pm
     attr_accessor :fi_over_sales
     attr_accessor :ni_over_sales
 
-    # Income Statement Ratios
+    # Income Statement Ratios (FIXME: this should be calc'ed here, so that it can be merged)
     attr_accessor :sales_over_noa # ATO
     attr_accessor :revenue_growth
     attr_accessor :core_oi_growth
@@ -37,6 +39,49 @@ module SecEdgar
     attr_accessor :re_oi # ReOI (at 10%)
 
     def initialize
+    end
+
+    def merge(fss2)
+
+      # choose which indices to use (not all reports will the same set of dates, or be sorted
+      # in the same direction.  Find common ones, then sort them ascending chronologically.)
+      c = fss2.report_dates - @report_dates
+      indices = c.collect { |x| fss2.report_dates.index(x) } # which indices to bring over
+
+      @report_dates += fss2.report_dates.values_at(*indices)
+
+      @oa  += fss2.oa.values_at(*indices)
+      @ol  += fss2.ol.values_at(*indices)
+      @noa += fss2.noa.values_at(*indices)
+      @fa  += fss2.fa.values_at(*indices)
+      @fl  += fss2.fl.values_at(*indices)
+      @nfa += fss2.nfa.values_at(*indices)
+      @cse += fss2.cse.values_at(*indices)
+
+      @composition_ratio += fss2.composition_ratio.values_at(*indices)
+      @noa_growth        += fss2.noa_growth.values_at(*indices)
+      @cse_growth        += fss2.cse_growth.values_at(*indices)
+
+      @operating_revenue       += fss2.operating_revenue.values_at(*indices)
+      @gross_margin            += fss2.gross_margin.values_at(*indices)
+      @oi_from_sales_after_tax += fss2.oi_from_sales_after_tax.values_at(*indices)
+      @oi_after_tax            += fss2.oi_after_tax.values_at(*indices)
+      @financing_income        += fss2.financing_income.values_at(*indices)
+      @net_income              += fss2.net_income.values_at(*indices)
+
+      @gm            += fss2.gm.values_at(*indices)
+      @sales_pm      += fss2.sales_pm.values_at(*indices)
+      @pm            += fss2.pm.values_at(*indices)
+      @fi_over_sales += fss2.fi_over_sales.values_at(*indices)
+      @ni_over_sales += fss2.fi_over_sales.values_at(*indices)
+
+      @sales_over_noa += fss2.sales_over_noa.values_at(*indices)
+      @revenue_growth += fss2.revenue_growth.values_at(*indices)
+      @core_oi_growth += fss2.core_oi_growth.values_at(*indices)
+      @oi_growth      += fss2.oi_growth.values_at(*indices)
+      @fi_over_nfa    += fss2.fi_over_nfa.values_at(*indices)
+      @re_oi          += fss2.re_oi.values_at(*indices)
+
     end
 
     def simple_valuation(g_1, g_2, g_long, rho_f)
@@ -50,7 +95,7 @@ module SecEdgar
       return v_e_0
     end
 
-    def write_summary(filename)
+    def to_csv(filename)
 
       CSV.open(filename, "wb") do |csv|
         csv << [""] + @report_dates
--- specs/sec_edgar_financial_statement_summary_spec.rb
+++ specs/sec_edgar_financial_statement_summary_spec.rb
@@ -7,7 +7,37 @@ describe SecEdgar::FinancialStatementSummary do
 
   before(:each) do
   end
-   
+      
+  describe "#merge" do
+    it "returns a simple valuation of the common equity" do
+      ticker = 'GOOG'
+      rept_type = '10-K'
+      download_path = "/tmp/"
+      edgar = SecEdgar::Edgar.new
+      
+      reports = edgar.lookup_reports(ticker)
+      reports.keep_if{ |r| r[:type] == rept_type }
+      reports.sort! {|a,b| a[:date] <=> b[:date] }
+      
+      files = edgar.get_reports(reports, download_path)
+
+      ten_k = SecEdgar::AnnualReport.new 
+      ten_k.parse(files.shift)
+      summary = ten_k.get_summary
+
+      while !files.empty?
+        ten_k2 = SecEdgar::AnnualReport.new 
+        ten_k2.parse(files.shift)
+        summary2 = ten_k2.get_summary
+  
+        summary.merge(summary2)
+      end
+
+      summary.report_dates.sort[0..7].should == ["2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010"] 
+      summary.to_csv("summary.csv")
+    end
+  end
+
   describe "#simple_valuation" do
     it "returns a simple valuation of the common equity" do
       ticker = 'GOOG'
